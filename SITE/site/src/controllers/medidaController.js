928a7d4707d35390ed2cfdbacadb3923c3663052
Yan Cesario <yan.cesario@sptech.school>
2023-10-23 01:10:25 -0300

    Correções no BD e backend na plotação de gráficos feitas

--- SITE/site/src/controllers/medidaController.js
+++ SITE/site/src/controllers/medidaController.js
@@ -6,7 +6,7 @@ function buscarUltimasMedidas(req, res) {
 
     console.log(`Recuperando as ultimas ${limite_linhas} medidas`);
 
-    medidaModel.buscarUltimasMedidas(idAquario, limite_linhas).then(function (resultado) {
+    medidaModel.buscarUltimasMedidas(limite_linhas).then(function (resultado) {
         if (resultado.length > 0) {
             res.status(200).json(resultado);
         } else {
