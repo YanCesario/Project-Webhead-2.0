e77a9a1f097fbb4686d90ac8058f88e080fca4c4
Yan Cesario <yan.cesario@sptech.school>
2023-10-24 03:20:46 -0300

    Código comentado e subindo arquivo certo do SQL

--- SQL/script projeto_individual.sql
+++ SQL/script projeto_individual.sql
@@ -1,34 +1,50 @@
-create database Aranhaverso;
-use Aranhaverso;
-
+create database aranhaverso;
+use aranhaverso;
 CREATE TABLE usuario (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(50),
 	email VARCHAR(50),
-	senha VARCHAR(50)
+	senha VARCHAR(50),
+	fkAranha int,
+    constraint fkAranha foreign key (fkAranha) references aranha(idAranha)
 );
+
+create table aranha (
+idAranha int primary key AUTO_INCREMENT,
+nome varchar(50)
+);
+create table pontuacao(
+	usuarioId int primary key,
+	fkAranhaUsuario int,
+	pontos int,
+	pedras int,
+	segundos int,
+	foreign key (fkAranhaUsuario)references usuario (fkAranha),
+	foreign key (usuarioId) references usuario(id)
+);
+
+insert into aranha(nome) value
+('Gwen'),
+('Miles'),
+('Noir'),
+('Porker'),
+('Miguel'),
+('Peni'),
+('Hobbie');
+    
+    drop view estatisticas_aranha;
+     CREATE VIEW estatisticas_aranha AS
+SELECT
+    a.nome AS nome_aranha,
+    SUM(p.pontos) AS total_pontos
+FROM
+    aranha a
+LEFT JOIN
+    pontuacao p ON a.idAranha = p.fkAranhaUsuario
+GROUP BY
+    a.nome;
+    
+    SELECT * FROM estatisticas_aranha;
 
 select * from usuario;
-
-create table universo(
-idUniverso int primary key auto_increment,
-Numero varchar(4)
-);
-
-create table aranha(
-idAranha INT PRIMARY KEY AUTO_INCREMENT,
-Nome VARCHAR(50),
-Descrição varchar(100),
- fkUniverso int,
- constraint fkUniverso foreign key (fkUniverso) references universo(idUniverso)
-);
-
-create table sociedadeAranha(
-idSociedade int,
-fkAranha int,
-constraint fkAranha foreign key (fkAranha) references aranha(idAranha),
-fkUniverso int,
-constraint fkUniverso foreign key (fkUniverso) references universo(idUniverso),
-pkComposta int,
-constraint pkComposta primary key (idSociedade, fkAranha, fkUniverso)
-);
+    
